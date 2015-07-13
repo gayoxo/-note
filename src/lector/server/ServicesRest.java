e73ffacb75e2af7d2ef8e3dd74c509c40ab3c33a
Joaquin Gayoso Cabada <gayoxo@gmail.com>
2015-07-13 15:51:39 +0200

    sfsdfsdf

--- src/lector/server/ServicesRest.java
+++ src/lector/server/ServicesRest.java
@@ -1,17 +1,21 @@
 package lector.server;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.servlet.ServletContext;
 import javax.transaction.UserTransaction;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
 
 import com.google.gson.Gson;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
@@ -25,7 +29,10 @@ public class ServicesRest  extends RemoteServiceServlet {
 	private EntityManagerFactory emf = Persistence
 			.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
 	
+	private static final String DATA_DIRECTORY = "data";
 	
+	@Context 
+	ServletContext context;
 	
 	
 	@Resource
@@ -34,7 +41,10 @@ public class ServicesRest  extends RemoteServiceServlet {
 		@Path("users")
 		@GET
 		public String doGreet() {
-			/*
+			
+			if (!isActiveUsersModule())
+			{
+			
 			try{
 				EntityManager entityManager = emf.createEntityManager();
 			List<UserApp> list=new ArrayList<UserApp>();
@@ -65,8 +75,10 @@ public class ServicesRest  extends RemoteServiceServlet {
 				e.printStackTrace();
 				return e.toString();
 			}
-			*/
-			return "Hola Mundo en mi servicio de Prueba";
+			
+		//	return "Hola Mundo en mi servicio de Prueba";
+			}
+			return "-1";
 		}
 		
 			@Path("getStructure/{colId_id}")
@@ -109,5 +121,36 @@ public class ServicesRest  extends RemoteServiceServlet {
 				}
 				*/
 				return "Hola Mundo en mi servicio de Prueba";
+			}
+			
+			
+			/**
+			 * Genera la carpeta de upload
+			 * 
+			 * @return
+			 */
+			private boolean isActiveUsersModule() {
+				String[] route = context.getRealPath("/").split(
+						Pattern.quote(File.separator));
+				// String[] route2=uri.getPath().split(Pattern.quote(File.separator));
+				// String[] route =
+				// getServletContext().getRealPath("").split(Pattern.quote(File.separator));
+				StringBuffer uploadFolderSb = new StringBuffer();
+				// uploadFolderSb.append(File.separator);
+				for (int i = 0; i < route.length - 2; i++) {
+					uploadFolderSb.append(route[i]);
+					uploadFolderSb.append(File.separator);
+				}
+
+				uploadFolderSb.append("docroot");
+				uploadFolderSb.append(File.separator + DATA_DIRECTORY + File.separator
+						+ "modules" + File.separator);
+				String uploadFolder = uploadFolderSb.toString();
+
+				java.io.File directorio = new File(uploadFolder);
+				directorio.mkdirs();
+				
+				java.io.File LogModule = new File(uploadFolder+"users.module");
+				return LogModule.exists();
 			}
 }
